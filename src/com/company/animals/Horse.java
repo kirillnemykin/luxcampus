17bcecb3d16fbeaf83957f6b3abb4723bf8b01f7
kirillnemykin <50525765+kirillnemykin@users.noreply.github.com>
2022-01-19 16:24:09 +0200

    Add files via upload

---
+++ src/com/company/animals/Horse.java
@@ -0,0 +1,37 @@
+package com.company.animals;
+
+public class Horse extends Animal {
+
+    private int speed;
+
+    public Horse(String name, int weight, int age, int speed) {
+        super(name, weight, age);
+        this.speed = speed;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Horse is making sound");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Horse is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Horse is sleeping");
+    }
+
+
+    @Override
+    public String toString() {
+        return "Horse{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                ", age=" + age +
+                ", speed=" + speed +
+                '}';
+    }
+}
