4a397cdb9ebd4390e733e1692fc965b01234cd2f
kirillnemykin <50525765+kirillnemykin@users.noreply.github.com>
2022-01-19 16:25:32 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import com.company.animals.Cat;
+import com.company.animals.Horse;
+import com.company.animals.Koala;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Cat", 5, 7, "black");
+        Cat cat2 = new Cat("Cat", 5, 7, "black");
+        Horse horse = new Horse("Horse1", 500, 10, 20);
+        Koala koala = new Koala("Koala1", 10, 5);
+        System.out.println(cat1);
+        cat1.makeSound();
+        System.out.println(cat2);
+        System.out.println("First cat equals to second cat?: " + cat1.equals(cat2));
+        System.out.println(horse);
+        horse.eat();
+        System.out.println(koala);
+        koala.sleep();
+    }
+}
+
